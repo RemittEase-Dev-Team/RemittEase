4215cc948df563f5292cc12cfe7ef6d597e4e86d
DoctorVee Orjiani <emekaorjiani@gmail.com>
2025-03-12 13:43:12 +0100

    moon pay test

---
+++ resources/js/Pages/MoonPay.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MoonPayBuyWidget, MoonPayProvider } from '@moonpay/moonpay-react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head, useForm } from '@inertiajs/react';
+
+export default function MoonPay() {
+    return (
+        <AuthenticatedLayout>
+            <div className="container mx-auto text-center bg-gray-800 bg-opacity-50">
+                <MoonPayProvider apiKey="pk_test_8v5c0U65vmujfNeSrA1b3hQSgTd9iE2" debug>
+                    <MoonPayBuyWidget
+                        variant="embedded"
+                        baseCurrencyCode="usd"
+                        baseCurrencyAmount="100"
+                        defaultCurrencyCode="xlm"
+                        visible={true}
+                    />
+                </MoonPayProvider>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,11 @@ Route::middleware([
     // 'verified',
     // 'kyc.verified'
     ])->group(function () {
+    Route::get('/moonpay', function () {
+        return Inertia::render('MoonPay');
+    });
+
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
